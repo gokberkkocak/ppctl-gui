70674c0d9e4fc35af4a62035f7df05f3380045fb
Gokberk Kocak <4727301+gokberkkocak@users.noreply.github.com>
2024-02-17 12:30:39 +0000

    add gtk wip

--- src/main.rs
+++ src/main.rs
@@ -1,120 +1,54 @@
-use eframe::egui::{self, Key};
+use gtk4::{self, glib, Adjustment, Application, ApplicationWindow, StateFlags};
+use gtk4::{prelude::*, Scale};
 
-use core::fmt::Display;
-use core::panic;
-use std::process::Command;
+const APP_ID: &str = "org.gtk_rs.MainEventLoop1";
 
-const POWER_SAVER: &str = "power-saver";
-const BALANCED: &str = "balanced";
-const PERFORMANCE: &str = "performance";
+fn main() -> glib::ExitCode {
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
 
-const PPCTL_CMD: &str = "powerprofilesctl";
-enum PowerProfile {
-    PowerSaver,
-    Balanced,
-    Performance,
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run()
 }
 
-impl From<String> for PowerProfile {
-    fn from(value: String) -> Self {
-        match value.trim() {
-            POWER_SAVER => PowerProfile::PowerSaver,
-            BALANCED => PowerProfile::Balanced,
-            PERFORMANCE => PowerProfile::Performance,
-            _ => panic!("cannot"),
-        }
-    }
-}
+fn build_ui(app: &Application) {
+    // Build slider
+    let slider = Scale::builder()
+        .orientation(gtk4::Orientation::Horizontal)
+        .digits(0)
+        .can_focus(false)
+        .adjustment(
+            &Adjustment::builder()
+                .value(1.0)
+                .lower(0.0)
+                .upper(2.0)
+                .step_increment(1.0)
+                .page_increment(0.0)
+                .page_size(0.0)
+                .build(),
+        )
+        .round_digits(0)
+        .build();
+    slider.set_value(1.0);
+    slider.add_mark(0.0, gtk4::PositionType::Bottom, Some("pp"));
+    slider.add_mark(1.0, gtk4::PositionType::Bottom, Some("b"));
+    slider.add_mark(2.0, gtk4::PositionType::Bottom, Some("per"));
 
-impl From<u8> for PowerProfile {
-    fn from(value: u8) -> Self {
-        match value {
-            0 => PowerProfile::PowerSaver,
-            1 => PowerProfile::Balanced,
-            2 => PowerProfile::Performance,
-            _ => panic!("cannot"),
-        }
-    }
-}
+    slider.connect_state_flags_changed(|s, _state| {
+        println!("{}", s.value())
+    });
 
-impl Default for PowerProfile {
-    fn default() -> Self {
-        PowerProfile::init()
-    }
-}
-
-impl Display for PowerProfile {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            PowerProfile::PowerSaver => write!(f, "{}", POWER_SAVER),
-            PowerProfile::Balanced => write!(f, "{}", BALANCED),
-            PowerProfile::Performance => write!(f, "{}", PERFORMANCE),
-        }
-    }
-}
-
-impl PowerProfile {
-    fn init() -> Self {
-        let output = Command::new(PPCTL_CMD)
-            .args(["get"])
-            .output()
-            .expect("failed to execute process");
-        let stdout = String::from_utf8(output.stdout).expect("cannot");
-        stdout.into()
-    }
-    fn to_u8(&self) -> u8 {
-        match self {
-            PowerProfile::PowerSaver => 0,
-            PowerProfile::Balanced => 1,
-            PowerProfile::Performance => 2,
-        }
-    }
-}
-
-#[derive(Default)]
-struct PPCtlGui {
-    state: PowerProfile,
-}
-
-impl eframe::App for PPCtlGui {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let cp = egui::CentralPanel::default();
-        let mut state_u8 = self.state.to_u8();
-        cp.show(ctx, |ui| {
-            let slider =
-                egui::Slider::new(&mut state_u8, 0..=2).custom_formatter(|s, _| match s as u64 {
-                    0 => POWER_SAVER.to_owned(),
-                    1 => BALANCED.to_owned(),
-                    2 => PERFORMANCE.to_owned(),
-                    _ => "".to_owned(),
-                });
-            ui.add(slider);
-            if self.state.to_u8() != state_u8 {
-                self.state = state_u8.into();
-                let _output = Command::new(PPCTL_CMD)
-                    .args(["set", &format!("{}", self.state)])
-                    .output()
-                    .expect("failed to execute process");
-                println!("Profile changed: {}", self.state);
-            }
-            if ctx.input(|i| i.key_pressed(Key::Escape)) {
-                ui.ctx().send_viewport_cmd(egui::ViewportCommand::Close);
-            }
-        });
-    }
-}
-
-fn main() -> eframe::Result<(), eframe::Error> {
-    // options
-    let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_app_id(env!("CARGO_CRATE_NAME"))
-            .with_inner_size([200.0, 35.0]),
-        ..Default::default()
-    };
-    eframe::run_native(
-        env!("CARGO_CRATE_NAME"),
-        options,
-        Box::new(|_cc| Box::<PPCtlGui>::default()),
-    )
+    // Create a window
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&slider)
+        .build();
+    window.set_default_size(200, 35);
+    
+    // Present window
+    window.present();
 }
